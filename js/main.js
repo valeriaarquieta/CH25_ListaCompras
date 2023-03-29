4e4d195347bed6b8ec82dc358deac492f9c0a955
baleriarg98 <valeria.arquietag@gmail.com>
2023-03-29 17:15:10 -0600

    Se comenzó la aplicación, se validan los campos y mensajes de error

--- js/main.js
+++ js/main.js
@@ -1,1 +1,43 @@
-// El código va aquí -> 
+let txtNombre=document.getElementById("Name");
+let txtNumber=document.getElementById("Number");
+
+let btnAgregar=document.getElementById("btnAgregar");
+let btnClear=document.getElementById("btnClear");
+
+let alertValidacionesTexto=document.getElementById("alertValidacionesTexto");
+let alertValidaciones=document.getElementById("alertValidaciones");
+
+btnClear.addEventListener("click",function(event){event.preventDefault();
+txtNombre.value="";
+txtNumber.value="";
+});
+
+btnAgregar.addEventListener("click",function(event){event.preventDefault();
+  let lista="Los siguientes campos estan mal:<ul>";
+ alertValidacionesTexto.innerHTML="";
+ alertValidaciones.style.display="none";
+
+
+    if(txtNombre.value.length==0){
+    txtNombre.style.border="solid thin red";
+    lista+="<li>Escribir nombre válido.</li>";
+    alertValidaciones.style.display="block";
+}else{txtNombre.style.border="";}
+
+if(txtNumber.value.length==0){
+    txtNumber.style.border="solid thin red";
+    lista+="<li>Escribir cantidad válida.</li>";
+    alertValidaciones.style.display="block";
+}else{txtNumber.style.border="";}
+
+lista+="<ul/>";
+alertValidacionesTexto.insertAdjacentHTML("beforeend",lista);
+});
+
+txtNumber.addEventListener("blur",function(event){event.preventDefault();
+txtNumber.value=txtNumber.value.trim();
+});
+
+txtNombre.addEventListener("blur",function(event){event.preventDefault();
+txtNombre.value=txtNombre.value.trim();
+});
