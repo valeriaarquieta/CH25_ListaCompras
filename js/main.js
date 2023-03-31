01193f095319b67a9d180660c0f0f4672e652e41
baleriarg98 <valeria.arquietag@gmail.com>
2023-03-31 12:56:32 -0600

    Se agregó la tabla aunque se reinicie la página y JSON.

--- js/main.js
+++ js/main.js
@@ -20,24 +20,31 @@ let precio = 0;
 let contador = 0;
 let totalEnProductos=0;
 let costoTotal=0;
+let datos=[]; //para los datos de la tabla
 
 //Limpiar los campos
 btnClear.addEventListener("click",function(event){
-event.preventDefault();
-txtNombre.value="";
-txtNumber.value="";
-cuerpoTabla[0].innerHTML="";
+// event.preventDefault();
+// txtNombre.value="";
+// txtNumber.value="";
+// cuerpoTabla[0].innerHTML="";
 
-contador = 0;
-totalEnProductos=0;
-costoTotal=0;
-contadorProductos.innerText="0";
-productosTotal.innerText="0";
-precioTotal.innerText="0";
+// contador = 0;
+// totalEnProductos=0;
+// costoTotal=0;
+// contadorProductos.innerText="0";
+// productosTotal.innerText="0";
+// precioTotal.innerText="0";
 
-localStorage.setItem("contadorProductos",contador);
-localStorage.setItem("totalEnProductos",totalEnProductos);
-localStorage.setItem("costoTotal",costoTotal.toFixed(2));
+let res=JSON.parse(localStorage.getItem("resumen"));
+let resumen =`{"contadorProductos": ${contador},
+"totalEnProductos": ${totalEnProductos},
+"costoTotal": ${costoTotal.toFixed(2)}}`;
+localStorage.setItem("resumen",resumen);
+
+// localStorage.setItem("contadorProductos",contador);
+// localStorage.setItem("totalEnProductos",totalEnProductos);
+// localStorage.setItem("costoTotal",costoTotal.toFixed(2));
 });//click btnClear
 
 function validarCantidad(){
@@ -92,7 +99,7 @@ btnAgregar.addEventListener("click",function(event){
   alertValidaciones.style.display="none";
   }, 5000);
   if (isValid){
-    precio = getPrecio();
+    precio = getPrecio(); 
     contador++;
     let row = `<tr> 
                   <th>${contador}</th>
@@ -101,15 +108,28 @@ btnAgregar.addEventListener("click",function(event){
                   <td>${precio}</td>
                </tr>`;
       
+      let elemento= `{ "id":${contador},
+                    "nombre":"${txtNombre.value}",
+                    "cantidad":"${txtNumber.value}",
+                    "precio":"${precio}"}`;
+datos.push(JSON.parse(elemento) );
+localStorage.setItem("datos",JSON.stringify(datos));
+
       cuerpoTabla[0].insertAdjacentHTML("beforeend", row);
       contadorProductos.innerText = contador;
       totalEnProductos+=parseFloat(txtNumber.value);
       productosTotal.innerText=totalEnProductos;
       costoTotal+=precio*parseFloat(txtNumber.value);
       precioTotal.innerText=`$ ${costoTotal.toFixed(2)}`;
-      localStorage.setItem("contadorProductos",contador);
-      localStorage.setItem("totalEnProductos",totalEnProductos);
-      localStorage.setItem("costoTotal",costoTotal.toFixed(2));
+
+      let resumen=`{"contadorProductos": ${contador},
+      "totalEnProductos": ${totalEnProductos},
+      "costoTotal": ${costoTotal.toFixed(2)}}`;
+      localStorage.setItem("resumen",resumen);
+
+      // localStorage.setItem("contadorProductos",contador);
+      // localStorage.setItem("totalEnProductos",totalEnProductos);
+      // localStorage.setItem("costoTotal",costoTotal.toFixed(2));
       txtNombre.value="";
       txtNumber.value="";
       txtNombre.focus();
@@ -123,24 +143,53 @@ txtNumber.addEventListener("blur",function(event){event.preventDefault();
 txtNumber.value=txtNumber.value.trim();
 });
 
-txtNombre.addEventListener("blur",function(event){event.preventDefault();
-txtNombre.value=txtNombre.value.trim();
-});
 window.addEventListener("load",function(event){
-if(localStorage.getItem("contadorProductos")==null)
-{localStorage.setItem("contadorProductos","0");
-
-}if(localStorage.getItem("totalEnProductos")==null){
-  localStorage.setItem("totalEnProductos","0");
-
-}if(localStorage.getItem("costoTotal")==null){
-  localStorage.setItem("costoTotal","0.0");}
-
-  contador= parseInt(localStorage.getItem("contadorProductos"));
-  totalEnProductos=parseInt(localStorage.getItem("totalEnProductos"));
-  costoTotal=parseFloat(localStorage.getItem("costoTotal","0.0"));
-
+  if(localStorage.getItem("resumen")==null){
+    let resumen=`{"contadorProductos":${contador}},
+    "totalEnProductos": ${totalEnProductos},
+    "costoTotal": ${costoTotal.toFixed(2)}}`;
+    localStorage.setItem("resumen",resumen);
+  } 
+  let res=JSON.parse(localStorage.getItem("resumen"));
+  
+  if(localStorage.getItem("datos")!=null){
+    datos=JSON.parse(localStorage.getItem("datos"));
+    datos.forEach(r => {
+      let row = `<tr> 
+      <th>${r.id}</th>
+      <td>${r.nombre}</td>
+      <td>${r.cantidad}</td>
+      <td>${r.precio}</td>
+   </tr>`;
+   cuerpoTabla[0].insertAdjacentHTML("beforeend", row);
+    });
+  }
+  contador=res.contadorProductos;
+  totalEnProductos=res.totalEnProductos;
+  costoTotal=res.costoTotal;
   contadorProductos.innerText=contador;
   productosTotal.innerText=totalEnProductos;
   precioTotal.innerText=`$ ${costoTotal}`;
+
+// {event.preventDefault();
+// txtNombre.value=txtNombre.value.trim();
+// });
+
+// window.addEventListener("load",function(event){
+// if(localStorage.getItem("contadorProductos")==null)
+// {localStorage.setItem("contadorProductos","0");
+
+// }if(localStorage.getItem("totalEnProductos")==null){
+//   localStorage.setItem("totalEnProductos","0");
+
+// }if(localStorage.getItem("costoTotal")==null){
+//   localStorage.setItem("costoTotal","0.0");}
+
+  // contador= parseInt(localStorage.getItem("contadorProductos"));
+  // totalEnProductos=parseInt(localStorage.getItem("totalEnProductos"));
+  // costoTotal=parseFloat(localStorage.getItem("costoTotal","0.0"));
+
+  // contadorProductos.innerText=contador;
+  // productosTotal.innerText=totalEnProductos;
+  // precioTotal.innerText=`$ ${costoTotal}`;
 });
